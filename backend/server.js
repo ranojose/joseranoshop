e28f92389f50fd6582fd261eeec4cd29cd635fc4
unknown <mojbudang@gmail.com>
2021-05-06 13:34:58 +0800

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-
- import colors from 'colors'
+import colors from 'colors'
  import morgan from 'morgan'
  import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -17,17 +16,13 @@ dotenv.config()
 connectDB()
  
 const app=express()
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 app.use(express.json())
 
-
-app.use((req, res, next) => {
-    console.log(req.originalUrl)
-    next()
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
